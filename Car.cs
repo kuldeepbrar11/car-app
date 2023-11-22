5ea410758332cb2f280ef1bc37647a9ea98ae6aa
Kuldeep Brar <71247863+kuldeepbrar11@users.noreply.github.com>
2023-11-21 22:08:14 -0500

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Car
+{
+    public class Car
+    {
+        public int year;
+        public string manufacturer;
+        public string model;
+        public double price;
+        public bool isDriveable;
+
+        public Car(int year, string manufacturer, string model, double price, bool isDrivable)
+        {
+            this.year = year;
+            this.manufacturer = manufacturer;
+            this.model = model;
+            this.price = price;
+            this.isDriveable = isDrivable;
+
+        }
+
+        public override string ToString()
+        {
+            string drivableStatus = isDriveable ? "Driveable" : "Not Driveable";
+            return $"Make of car is {year}, manufactured by {manufacturer}, Model is {model}, Can this car be driven? {isDriveable}, it's price is {price}";
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Car;
+
+class Program
+{
+    static void Main()
+    {
+        Car car1 = new Car(2014, "Honda", "Amaze", 15000, true);
+        Car car2 = new Car(2018, "Toyota", "Camry", 25000, true);
+        Car car3 = new Car(2020, "Ford", "Mustang", 40000, true);
+        Car car4 = new Car(2010, "Honda", "City", 8000, false);
+
+        Console.WriteLine(car1);
+        Console.WriteLine(car2);
+        Console.WriteLine(car3);
+        Console.WriteLine(car4);
+    }
+}
+
